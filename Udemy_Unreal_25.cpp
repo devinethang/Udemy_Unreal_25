be7018c45151799765b071a56fb535b8c16d3144
Devin Graupmann <devinethang@gmail.com>
2019-09-01 13:55:36 -0700

    display a message

--- Udemy_Unreal_25.cpp
+++ Udemy_Unreal_25.cpp
@@ -1,11 +1,29 @@
-// Udemy_Unreal_25.cpp : Defines the entry point for the console application.
+// Udemy_Unreal_24.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
 
 
 int main()
 {
-    return 0;
-}
+	cout << "Enter a positive or negative integer: ";
+	int input;
+	cin >> input;
+	if (input == 0)
+	{
+		cout << "You entered zero." << endl;
+	}
+	else if (input < 0)
+	{
+		cout << "You entered a number less than zero." << endl;
+	}
+	else
+	{
+		cout << "You entered a number above zero." << endl;
+	}
 
+	return 0;
+}
